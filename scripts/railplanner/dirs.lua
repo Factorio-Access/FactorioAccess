f977c37d9caf3c98cfe011cd02211a4bfc44a5b6
Gweneph <12696697+EphDoering@users.noreply.github.com>
2025-03-09 14:50:43 -0400

    fixed formatting

--- scripts/railplanner/dirs.lua
+++ scripts/railplanner/dirs.lua
@@ -1,24 +1,23 @@
-
-local dirs=defines.direction
+local dirs = defines.direction
 
 --1.0 and 2.0 compatible directions. In 1.0 the new directions will be wrong but not error producing
 ---@enum dirs16
-local dirs16={
-   north=dirs.north,
-   northnortheast=dirs.north+1,
-   northeast=dirs.northeast,
-   eastnortheast=dirs.northeast+1,
-   east=dirs.east,
-   eastsoutheast=dirs.east+1,
-   southeast=dirs.southeast,
-   southsoutheast=dirs.southeast+1,
-   south=dirs.south,
-   southsouthwest=dirs.south+1,
-   southwest=dirs.southwest,
-   westsouthwest=dirs.southwest+1,
-   west=dirs.west,
-   westnorthwest=dirs.west+1,
-   northwest=dirs.northwest,
-   northnorthwest=(dirs.northwest+1) % (2*dirs.south)
+local dirs16 = {
+   north = dirs.north,
+   northnortheast = dirs.north + 1,
+   northeast = dirs.northeast,
+   eastnortheast = dirs.northeast + 1,
+   east = dirs.east,
+   eastsoutheast = dirs.east + 1,
+   southeast = dirs.southeast,
+   southsoutheast = dirs.southeast + 1,
+   south = dirs.south,
+   southsouthwest = dirs.south + 1,
+   southwest = dirs.southwest,
+   westsouthwest = dirs.southwest + 1,
+   west = dirs.west,
+   westnorthwest = dirs.west + 1,
+   northwest = dirs.northwest,
+   northnorthwest = (dirs.northwest + 1) % (2 * dirs.south),
 }
 return dirs16
--- scripts/railplanner/priorityqueue.lua
+++ scripts/railplanner/priorityqueue.lua
@@ -14,7 +14,8 @@ License: zlib
   2. Altered source versions must be plainly marked as such, and must not be
      misrepresented as being the original software.
   3. This notice may not be removed or altered from any source distribution.
-]]--
+]]
+--
 -- modified by xxopxe@gmail.com
 
 local floor = math.floor
@@ -23,112 +24,110 @@ local floor = math.floor
 local PriorityQueue = {}
 PriorityQueue.__index = PriorityQueue
 
-setmetatable(
-  PriorityQueue,
-  {
-    __call = function ()
+setmetatable(PriorityQueue, {
+   __call = function()
       local new = {}
       setmetatable(new, PriorityQueue)
       new:initialize()
       return new
-    end
-  }
-)
-
+   end,
+})
 
 function PriorityQueue:initialize()
-  --[[  Initialization.
+   --[[  Initialization.
     Example:
         PriorityQueue = require("priority_queue")
         pq = PriorityQueue()
-    ]]--
-  self.heap_val = {}
-  self.heap_pri = {}
-  self.current_size = 0
+    ]]
+   --
+   self.heap_val = {}
+   self.heap_pri = {}
+   self.current_size = 0
 end
 
 function PriorityQueue:empty()
-  return self.current_size == 0
+   return self.current_size == 0
 end
 
 function PriorityQueue:size()
-  return self.current_size
+   return self.current_size
 end
 
 function PriorityQueue:swim()
-  -- Swim up on the tree and fix the order heap property.
-  local heap_val = self.heap_val
-  local heap_pri = self.heap_pri
-  local floor = floor
-  local i = self.current_size
+   -- Swim up on the tree and fix the order heap property.
+   local heap_val = self.heap_val
+   local heap_pri = self.heap_pri
+   local floor = floor
+   local i = self.current_size
 
-  while floor(i / 2) > 0 do
-    local half = floor(i / 2)
-    if heap_pri[i] < heap_pri[half] then
-      heap_val[i], heap_val[half] = heap_val[half], heap_val[i]
-      heap_pri[i], heap_pri[half] = heap_pri[half], heap_pri[i]
-    end
-    i = half
-  end
+   while floor(i / 2) > 0 do
+      local half = floor(i / 2)
+      if heap_pri[i] < heap_pri[half] then
+         heap_val[i], heap_val[half] = heap_val[half], heap_val[i]
+         heap_pri[i], heap_pri[half] = heap_pri[half], heap_pri[i]
+      end
+      i = half
+   end
 end
 
 function PriorityQueue:put(v, p)
-  --[[ Put an item on the queue.
+   --[[ Put an item on the queue.
     Args:
         v: the item to be stored
         p(number): the priority of the item
-    ]]--
-  --
-  self.current_size = self.current_size + 1
-  self.heap_val[self.current_size] = v
-  self.heap_pri[self.current_size] = p
-  self:swim()
+    ]]
+   --
+   --
+   self.current_size = self.current_size + 1
+   self.heap_val[self.current_size] = v
+   self.heap_pri[self.current_size] = p
+   self:swim()
 end
 
 function PriorityQueue:sink()
-  -- Sink down on the tree and fix the order heap property.
-  local size = self.current_size
-  local heap_val = self.heap_val
-  local heap_pri = self.heap_pri
-  local i = 1
+   -- Sink down on the tree and fix the order heap property.
+   local size = self.current_size
+   local heap_val = self.heap_val
+   local heap_pri = self.heap_pri
+   local i = 1
 
-  while (i * 2) <= size do
-    local mc = self:min_child(i)
-    if heap_pri[i] > heap_pri[mc] then
-      heap_val[i], heap_val[mc] = heap_val[mc], heap_val[i]
-      heap_pri[i], heap_pri[mc] = heap_pri[mc], heap_pri[i]
-    end
-    i = mc
-  end
+   while (i * 2) <= size do
+      local mc = self:min_child(i)
+      if heap_pri[i] > heap_pri[mc] then
+         heap_val[i], heap_val[mc] = heap_val[mc], heap_val[i]
+         heap_pri[i], heap_pri[mc] = heap_pri[mc], heap_pri[i]
+      end
+      i = mc
+   end
 end
 
 function PriorityQueue:min_child(i)
-  if (i * 2) + 1 > self.current_size then
-    return i * 2
-  else
-    if self.heap_pri[i * 2] < self.heap_pri[i * 2 + 1] then
+   if (i * 2) + 1 > self.current_size then
       return i * 2
-    else
-      return i * 2 + 1
-    end
-  end
+   else
+      if self.heap_pri[i * 2] < self.heap_pri[i * 2 + 1] then
+         return i * 2
+      else
+         return i * 2 + 1
+      end
+   end
 end
 
 function PriorityQueue:pop()
-  -- Remove and return the top priority item
-  local heap_val = self.heap_val
-  local heap_pri = self.heap_pri
-  local retval, retprio = heap_val[1], heap_pri[1]
-  heap_val[1], heap_pri[1] = heap_val[self.current_size], heap_pri[self.current_size]
-  heap_val[self.current_size], heap_pri[self.current_size] = nil, nil
-  self.current_size = self.current_size - 1
-  self:sink()
-  return retval, retprio
+   -- Remove and return the top priority item
+   local heap_val = self.heap_val
+   local heap_pri = self.heap_pri
+   local retval, retprio = heap_val[1], heap_pri[1]
+   heap_val[1], heap_pri[1] = heap_val[self.current_size], heap_pri[self.current_size]
+   heap_val[self.current_size], heap_pri[self.current_size] = nil, nil
+   self.current_size = self.current_size - 1
+   self:sink()
+   return retval, retprio
 end
 
 function PriorityQueue:peek()
-  -- return the top priority item
-  return self.heap_val[1], self.heap_pri[1]
+   -- return the top priority item
+   return self.heap_val[1], self.heap_pri[1]
 end
 
 return PriorityQueue
