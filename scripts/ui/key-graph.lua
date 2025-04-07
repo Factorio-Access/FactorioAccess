13b0a2a9c58fc5ee086addb29fe4035222c800ae
Austin Hicks <ahicks@ahicks.io>
2025-04-06 19:16:37 -0700

    Introduce key-graph, a UI assembly language, and reimplement the blueprint menu on it

---
+++ scripts/ui/key-graph.lua
@@ -0,0 +1,379 @@
+--[[
+This is UI "assembly language".  You probably want a higher level builder e.g. MenuBuilder, coupled with
+declare_keygraph from this file.  This comment primarily talks about what this is and how it works, but how to use it is
+different--see blueprints menu for a good example.  Read on if you need to add new UI kinds (not new menus, but e.g. the
+concept of a menu itself).
+
+# Overview and the Problem
+
+Factorio Access has a problem.  Unlike most UI, we must invent a mechanism to expose fast keyboard-based navigation even
+potentially at the cost of learnability.  The naive approach makes it look like you just have some sort of vertical
+and/or horizontal menu, but a closer look indicates this is not the case.  To name a couple, the "signal selector"
+(inserters and circuit network) is an odd sort of tree with weird rules, and the crafting and research "menus" are
+actually categorized horizontal rows of different lengths.  Then the belt analyzer is a simple grid--but not, because it
+has unique coordinate messaging per cell plus rules about what to say on a transition when going "sideways" from lane to
+lane.  It's also trivially easy to conceptualize more that we would want if we could ghet them, but we can't so
+everything gets turned into menus or non-UI hotkeys.
+
+After much thought, one can observe that all UI can be represented as:
+
+- Some tabs, not represented in this file (see tab-list.lua).
+- A set of controls, each with a label and possible associated sounds.
+- A set of transitions between controls.  The majority are silent and speechless, but some (e.g. transport belts) want
+  to add text that only speaks when transitioning.
+- The ability to search the menu.
+
+# The Solution
+
+There's lots of things one could conceive of.  Maybe you've got a "form" with "buttons".  Or a vertical menu.  ANd you
+call all of these controls.  Maybe you have custom key handling that forms a "UI" with custom drawing code.
+
+Or, you could just list all the nodes and transitions out, then write helpers that can more easily build complex
+structures like menus.
+
+To that end: this is a directed graph.  Nodes are controls.  Each node can have up to 4 transitions up/down/left/right.
+Nodes and transitions each get a set of properties saying what to do.  For messaging, this is usually in the form of a
+function which gets passed a `fa.ui.GraphCtx` to append to.  Others represent actions like gain_focus and left_click
+([).
+
+The graph itself comes from a function returning the graph.  This function may return a top-level constant graph or it
+may return a dynamic one generated for just this call.  In the latter case, one can capture entities.  It is guaranteed
+that the graph is re-rendered before the return value is used, and that the closures so returned will be thrown out.
+That means that for example one can capture what's in the player's hand and pre-check validity.  That's not good enough
+for all cases though, so the context also contains `parameters` and `graph_state`.  A lesser key `state` is maintained
+per-node and/or per-transition so that things can be reused: things that don't need the whole thing can store there.
+using parameters and states, it is possible to make graphs once and return them, communicating with the nodes via the
+state.  That is how we handle more performance sensitive paths.
+
+The graph can close itself (the graph context contains a controller, to tell the graph what to do).  Alternatively if
+render returns nil, the graph closes.  Since it is guaranteed that render is always called before the rest and that
+there is no gap between render and using the value it returns, performing checks at the top of render is sufficient and
+they do not necessarily need to propagate to the rest of it.
+
+Here is the trick for search: start at the graph's start node and then traverse outward to gather all keys and search
+labels, with a heuristic that prefers right/down first over up/left.  That'll order correctly for all UIs as of
+2024-04-06.
+
+And here is the trick for graphical rendering: rendering is simpler.  If a graph supports rendering the "shape" can be
+inferred from right/down connections, each as a box.  That won't capture weird up/left transitions but will capture the
+case of grids, menus, categorized named rows, etc.  We just have to possibly implement a "graphical type" and tag UIs
+with it.  For example, the signal selector is very complicated from the blind person's perspective, but from a graphical
+perspective it's just a normal tree.
+
+# The down-right contstraint
+
+Graphs must support modification out from under the player e.g. because a blueprint got imported or because someone else
+changed values.  They must also support search.  In both cases one must define a total order over the graph's nodes (if
+it is unobvious why you need that for modification, it's because the player goes to the "most valid" key).
+
+We define this total order as follows: go right until not possible.  Go down from everything we just found.  For each go
+right until not possible...etc.  This naturally suits most UIs because it is usually up/left that get weird, e.g. a
+tree's children all going up to a single parent.
+
+*but* if there is a node which may only be reached via an up or a left, none of this works.  So the down-right
+constraint is this: going down and right must visit the whole graph eventually.  It can have cycles.  Up and left can do
+whatever they want.  But going down and right must visit all nodes.  Generally this "just works out" because it aligns
+naturally with how the UX for a UI should be.
+]]
+local UiSounds = require("scripts.ui.sounds")
+local TH = require("scripts.table-helpers")
+
+local mod = {}
+
+---@alias fa.ui.graph.Modifiers { control: boolean, alt: boolean, shift: boolean }
+
+---@class fa.ui.graph.Ctx
+---@field message fa.MessageBuilder
+---@field modifiers fa.ui.graph.Modifiers Set for things using the keyboard. Non-nil but garbage for all others.
+---@field state any
+---@field pindex number
+
+---@alias fa.ui.graph.SimpleCallback fun(fa.ui.GraphCtx)
+
+---@class fa.ui.graph.NodeVtable
+---@field label fa.ui.graph.SimpleCallback Required always.
+---@field left_click fa.ui.graph.SimpleCallback? By default, re-say the label instead.
+
+---@class fa.ui.graph.TransitionVtable
+---@field label fa.ui.graph.SimpleCallback?
+---@field play_sound fa.ui.graph.SimpleCallback?
+
+---@class fa.ui.graph.Transition
+---@field destination string
+---@field vtable fa.ui.graph.TransitionVtable
+
+---@enum fa.ui.graph.TransitionDir
+mod.TRANSITION_DIR = {
+   UP = 0,
+   RIGHT = 1,
+   DOWN = 2,
+   LEFT = 3,
+}
+
+---@class fa.ui.graph.Node
+---@field vtable fa.ui.graph.NodeVtable
+---@field transitions table<fa.ui.graph.TransitionDir, fa.ui.graph.Transition>
+
+---@class fa.ui.graph.Render
+---@field nodes table<string, fa.ui.graph.Node>
+---@field start_key string
+
+---@class fa.ui.graph.StoredState
+---@field cur_key string
+---@field key_order string[]? Not set the first time through.
+
+---@class fa.ui.graph.InternalTabCtx: fa.ui.TabContext
+---@field state fa.ui.graph.StoredState
+
+---@alias fa.ui.graph.RenderCallback fun(fa.ui.graph.Ctx)
+
+---@class fa.ui.Graph: fa.ui.TabCallbacks
+---@field render fa.ui.graph.Render
+---@field render_callback fa.ui.graph.RenderCallback
+local Graph = {}
+local Graph_meta = { __index = Graph }
+
+---@class fa.ui.graph.Controller
+---@field graph fa.ui.Graph
+---@field ctx fa.ui.graph.InternalTabCtx
+local Controller = {}
+local Controller_meta = { __index = Controller }
+
+function Controller:close()
+   self.ctx.force_close = true
+end
+
+function Controller:close_for_textbox()
+   self.ctx.force_close = true
+   self.ctx.close_is_textbox = true
+end
+
+---@type fa.ui.graph.Modifiers
+local NO_MODIFIERS = { control = false, alt = false, shift = false }
+
+---@param  outer_ctx fa.ui.graph.InternalTabCtx
+---@param modifiers fa.ui.graph.Modifiers
+---@return fa.ui.graph.Ctx
+---@private
+function Graph:_wrap_ctx(outer_ctx, modifiers)
+   local controller = setmetatable({
+      graph = self,
+      ctx = outer_ctx,
+   }, Controller_meta)
+
+   ---@type fa.ui.graph.Ctx
+   local inner_ctx = {
+      message = outer_ctx.message,
+      modifiers = modifiers,
+      controller = controller,
+      state = outer_ctx.state,
+      pindex = outer_ctx.pindex,
+   }
+
+   return inner_ctx
+end
+
+---@param ctx fa.ui.graph.InternalTabCtx
+---@private
+function Graph:_rerender(ctx)
+   local render = self.render_callback(self:_wrap_ctx(ctx, NO_MODIFIERS))
+   if not render then
+      ctx.force_close = true
+      return
+   end
+
+   self.render = render
+
+   local state = ctx.state
+   local prev_key_order = state.key_order
+
+   -- If the key is no longer present, find the closest key to it which is, then move the focus to there.  Otherwise
+   -- move the focus to the start node.
+   if not render.nodes[state.cur_key] then
+      if not state.key_order then
+         state.cur_key = render.start_key
+      else
+         local old_ind = TH.find_index_of(state.key_order, state.cur_key)
+         assert(old_ind, "cur_key should always have been in the previous version of the graph")
+         for i = old_ind, 1, -1 do
+            local k = state.key_order[i]
+            if render.nodes[k] then
+               state.cur_key = k
+               break
+            end
+         end
+      end
+
+      if not state.cur_key then state.cur_key = render.start_key end
+   end
+
+   -- Perform a loop-based graph traversal to compute the new key order.
+   ---@type string[]
+   local down_fringe = { render.start_node }
+
+   ---@type table<string, boolean>
+   local seen = {}
+   local new_order = {}
+
+   local tins = table.insert
+
+   local i = 1
+   while i <= #down_fringe do
+      local k = down_fringe[i]
+      -- The fringe is always unique. Let's check that--it will find bugs if not.
+      assert(not seen[k])
+
+      -- Everything to the right goes into the order.  Everything to the down goes into the fringe.  This loop started
+      -- at a down, so add it now; then go right.
+      while not seen[k] do
+         seen[k] = true
+         tins(new_order, k)
+
+         local n = render.nodes[k]
+         local t = n.transitions[mod.TRANSITION_DIR.RIGHT]
+         local d = n.transitions[mod.TRANSITION_DIR.DOWN]
+
+         -- If there is a down we must explore it later on.
+         if d then tins(down_fringe, d.destination) end
+
+         -- We've gone as far right as we can.
+         if not t then break end
+
+         local nk = t.destination
+
+         k = nk
+      end
+
+      -- Fringe only contains downs. Move to the next "level".
+      i = i + 1
+   end
+
+   state.key_order = new_order
+end
+
+-- Call the callback if and only if a re-render succeeded.
+---@param ctx fa.ui.graph.InternalTabCtx
+---@param callback fun(): any
+---@private
+function Graph:_with_render(ctx, callback)
+   self:_rerender(ctx)
+   if ctx.force_close then return end
+   return callback()
+end
+
+-- When a callback defaults to another callback it gets an entry in this table.
+local CALLBACK_FALLBACKS = {
+   on_click = "label",
+}
+
+---@param node { vtable: any }
+---@param outer_ctx fa.ui.graph.InternalTabCtx
+---@param callback_name string
+---@param modifiers fa.ui.graph.Modifiers
+---@param ... any
+---@private
+---@return any?
+function Graph:_maybe_call(node, outer_ctx, callback_name, modifiers, ...)
+   if outer_ctx.force_close then return end
+   local inner_ctx = self:_wrap_ctx(outer_ctx, modifiers)
+
+   local fallback_callback_name = CALLBACK_FALLBACKS[callback_name]
+   local sound = function() end
+   local cb = node.vtable[callback_name]
+   if not cb then cb = node.vtable[fallback_callback_name] end
+
+   if cb then return cb(inner_ctx, ...) end
+end
+
+---@private
+---@param ctx fa.ui.graph.InternalTabCtx
+function Graph:_do_move(ctx, dir)
+   self:_with_render(ctx, function()
+      local state = ctx.state
+      local render = self.render
+      local node = render.nodes[state.cur_key]
+      local t = node.transitions[dir]
+      local new_node = node
+      if t then
+         new_node = render.nodes[t.destination]
+         assert(new_node)
+      end
+
+      -- Didn't move. Behavior: play a click.
+      if node == new_node then
+         UiSounds.play_menu_move(ctx.pindex)
+         self:_maybe_call(new_node, ctx, "label", NO_MODIFIERS)
+         return
+      end
+
+      self:_maybe_call(t, ctx, "label", NO_MODIFIERS)
+      self:_maybe_call(t, ctx, "play_sound", NO_MODIFIERS)
+      self:_maybe_call(new_node, ctx, "label", NO_MODIFIERS)
+      state.cur_key = t.destination
+   end)
+end
+
+---@param ctx fa.ui.graph.InternalTabCtx
+function Graph:on_click(ctx)
+   self:_with_render(ctx, function()
+      local n = self.render.nodes[ctx.state.cur_key]
+      -- Right now we only have lefft click with no modifiers.
+      self:_maybe_call(n, ctx, "on_click", NO_MODIFIERS)
+   end)
+end
+
+---@param ctx fa.ui.graph.InternalTabCtx
+function Graph:on_left(ctx)
+   self:_do_move(ctx, mod.TRANSITION_DIR.LEFT)
+end
+
+---@param ctx fa.ui.graph.InternalTabCtx
+function Graph:on_right(ctx)
+   self:_do_move(ctx, mod.TRANSITION_DIR.RIGHT)
+end
+
+---@param ctx fa.ui.graph.InternalTabCtx
+function Graph:on_up(ctx)
+   self:_do_move(ctx, mod.TRANSITION_DIR.UP)
+end
+
+---@param ctx fa.ui.graph.InternalTabCtx
+function Graph:on_down(ctx)
+   self:_do_move(ctx, mod.TRANSITION_DIR.DOWN)
+end
+
+---@param ctx fa.ui.graph.InternalTabCtx
+function Graph:on_tab_list_opened(ctx)
+   self:_with_render(ctx, function()
+      ctx.state.cur_key = self.render.start_key
+   end)
+end
+
+---@param ctx fa.ui.graph.InternalTabCtx
+function Graph:on_tab_focused(ctx)
+   self:_with_render(ctx, function()
+      local node = self.render.nodes[ctx.state.cur_key]
+      self:_maybe_call(node, ctx, "label", NO_MODIFIERS)
+   end)
+end
+
+---@class fa.ui.graph.Declaration
+---@field title LocalisedString
+---@field render_callback fa.ui.graph.RenderCallback
+---@field name string
+
+---@param declaration fa.ui.graph.Declaration
+---@return fa.ui.TabDescriptor
+function mod.declare_graph(declaration)
+   local graph = setmetatable({
+      render_callback = declaration.render_callback,
+   }, Graph_meta)
+
+   return {
+      name = declaration.name,
+      title = declaration.title,
+      callbacks = graph,
+   }
+end
+
+return mod
--- scripts/ui/menus/blueprints-menu.lua
+++ scripts/ui/menus/blueprints-menu.lua
@@ -1,5 +1,4 @@
 local Menu = require("scripts.ui.menu")
-local MenuItems = require("scripts.ui.menu-items")
 local TabList = require("scripts.ui.tab-list")
 local Functools = require("scripts.functools")
 local Graphics = require("scripts.graphics")
@@ -7,16 +6,12 @@ local Blueprints = require("scripts.blueprints")
 local Localising = require("scripts.localising")
 local TH = require("scripts.table-helpers")
 local FaUtils = require("scripts.fa-utils")
+local UiKeyGraph = require("scripts.ui.key-graph")
 local UiRouter = require("scripts.ui.router")
 
 local mod = {}
 
-local function state(ctx)
-   -- We don't use this; it relies on the blueprint in hand.
-   return {}
-end
-
----@type fun(fa.MenuCtx): fa.MenuRender?
+---@type fun(fa.ui.graph.Ctx): fa.ui.graph.Render?
 local function render(ctx)
    local p = game.get_player(ctx.pindex)
    if not p then return nil end
@@ -25,119 +20,101 @@ local function render(ctx)
    local bp = p.cursor_stack
    if not bp.is_blueprint then return nil end
 
-   local menu_items = {}
+   local builder = Menu.MenuBuilder.new()
 
    -- In the below note that it is long-standing mod behavior to close menus
    -- after text boxes close.  That's unfortunate, but we'll keep doing that for
    -- now as we flesh out our gui story.
 
    if not bp.is_blueprint_setup() then
-      table.insert(menu_items, MenuItems.simple_label("blueprint-info", { "fa.ui-blueprints-menu-limited" }))
+      builder:add_label("blueprint-info", { "fa.ui-blueprints-menu-limited" })
    else
-      table.insert(
-         menu_items,
-         MenuItems.simple_label("blueprint-info", { "fa.ui-blueprints-menu-basic", Blueprints.get_blueprint_label(bp) })
-      )
+      builder:add_label("blueprint-info", { "fa.ui-blueprints-menu-basic", Blueprints.get_blueprint_label(bp) })
    end
 
    -- Blueprints which are empty can't have descriptions; don't show it.
    if bp.is_blueprint_setup then
-      table.insert(
-         menu_items,
-         MenuItems.lazy_label("description", function(ctx)
-            ctx.message:fragment({ "fa.ui-blueprints-menu-description", Blueprints.get_blueprint_description(bp) })
+      builder:add_label("description", function(ctx)
+         ctx.message:fragment({ "fa.ui-blueprints-menu-description", Blueprints.get_blueprint_description(bp) })
+      end)
+
+      builder:add_label("icons", function(ctx)
+         local icons = bp.preview_icons
+         if not icons or not next(icons) then
+            ctx.message:fragment({ "fa.ui-blueprints-menu-no-icons" })
+            return
+         end
+
+         table.sort(icons, function(a, b)
+            return a.index < b.index
          end)
-      )
 
-      table.insert(
-         menu_items,
-         MenuItems.lazy_label("icons", function(ctx)
-            local icons = bp.preview_icons
-            if not icons or not next(icons) then
-               ctx.message:fragment({ "fa.ui-blueprints-menu-no-icons" })
-               return
-            end
+         local icons = TH.map(bp.preview_icons, function(icon)
+            -- Localising this is tricky because it could be from anywhere.
 
-            table.sort(icons, function(a, b)
-               return a.index < b.index
-            end)
+            local proto = FaUtils.find_prototype(icon.signal.name)
+            if proto then return Localising.get_localised_name_with_fallback(proto) end
 
-            local icons = TH.map(bp.preview_icons, function(icon)
-               -- Localising this is tricky because it could be from anywhere.
-
-               local proto = FaUtils.find_prototype(icon.signal.name)
-               if proto then return Localising.get_localised_name_with_fallback(proto) end
-
-               return icon.signal.name
-            end)
-
-            ctx.message:fragment({ "fa.ui-blueprints-menu-icons" })
-            for _, i in ipairs(icons) do
-               ctx.message:list_item(i)
-            end
+            return icon.signal.name
          end)
-      )
 
-      table.insert(
-         menu_items,
-         MenuItems.lazy_label("count-and-dims", function(ctx)
-            local count = bp.get_blueprint_entity_count()
-            local width, height = Blueprints.get_blueprint_width_and_height(pindex)
-            ctx.message:fragment({ "fa.ui-blueprints-menu-count-and-dims", width, height, count })
+         ctx.message:fragment({ "fa.ui-blueprints-menu-icons" })
+         for _, i in ipairs(icons) do
+            ctx.message:list_item(i)
+         end
+      end)
+
+      builder:add_label("count-and-dims", function(ctx)
+         local count = bp.get_blueprint_entity_count()
+         local width, height = Blueprints.get_blueprint_width_and_height(ctx.pindex)
+         ctx.message:fragment({ "fa.ui-blueprints-menu-count-and-dims", width, height, count })
+      end)
+
+      builder:add_label("components", function(ctx)
+         local comps = bp.get_blueprint_entities()
+         if not comps or not next(comps) then
+            ctx.message:fragment({ "fa.ui-blueprints-menu-no-components" })
+            return
+         end
+
+         ctx.message:fragment({ "fa.ui-blueprints-menu-components-intro" })
+         local counts = {}
+         for _, comp in ipairs(comps) do
+            counts[comp.name] = (counts[comp.name] or 0) + 1
+         end
+
+         local by_name = TH.set_to_sorted_array(counts)
+         print(serpent.line(by_name))
+         table.sort(by_name, function(a, b)
+            return a[2] > b[2]
          end)
-      )
 
-      table.insert(
-         menu_items,
-         MenuItems.lazy_label("components", function(ctx)
-            local comps = bp.get_blueprint_entities()
-            if not comps or not next(comps) then
-               ctx.message:fragment({ "fa.ui-blueprints-menu-no-components" })
-               return
-            end
+         for _, ent in pairs(by_name) do
+            local comp, count = ent[1], ent[2]
+            ctx.message:fragment(Localising.get_localised_name_with_fallback(prototypes.entity[comp]))
+         end
+      end)
 
-            ctx.message:fragment({ "fa.ui-blueprints-menu-components-intro" })
-            local counts = {}
-            for _, comp in ipairs(comps) do
-               counts[comp.name] = (counts[comp.name] or 0) + 1
-            end
-
-            local by_name = TH.set_to_sorted_array(counts)
-            print(serpent.line(by_name))
-            table.sort(by_name, function(a, b)
-               return a[2] > b[2]
-            end)
-
-            for _, ent in pairs(by_name) do
-               local comp, count = ent[1], ent[2]
-               ctx.message:fragment(Localising.get_localised_name_with_fallback(prototypes.entity[comp]))
-            end
-         end)
-      )
-
-      table.insert(
-         menu_items,
-         MenuItems.clickable_label("rename", { "fa.ui-blueprints-menu-rename" }, function(ctx)
+      builder:add_clickable("rename", { "fa.ui-blueprints-menu-rename" }, {
+         on_click = function(ctx)
             storage.players[ctx.pindex].blueprint_menu.edit_label = true
             Graphics.create_text_field_frame(ctx.pindex, "blueprint-edit-label")
             ctx.message:fragment({ "fa.ui-blueprints-rename-txtbox" })
-            ctx.controller:close_because_textbox()
-         end)
-      )
+            ctx.controller:close_for_textbox()
+         end,
+      })
 
-      table.insert(
-         menu_items,
-         MenuItems.clickable_label("edit-desc", { "fa.ui-blueprints-menu-edit-desc" }, function(ctx)
+      builder:add_clickable("edit-desc", { "fa.ui-blueprints-menu-edit-desc" }, {
+         on_click = function(ctx)
             storage.players[ctx.pindex].blueprint_menu.edit_description = true
             Graphics.create_text_field_frame(ctx.pindex, "blueprint-edit-description")
             ctx.message:fragment({ "fa.ui-blueprints-description-txtbox" })
-            ctx.controller:close_because_textbox()
-         end)
-      )
+            ctx.controller:close_for_textbox()
+         end,
+      })
 
-      table.insert(
-         menu_items,
-         MenuItems.clickable_label("copy", { "fa.ui-blueprints-menu-copy" }, function(ctx)
+      builder:add_clickable("copy", { "fa.ui-blueprints-menu-copy" }, {
+         on_click = function(ctx)
             local p = game.get_player(ctx.pindex)
             if not p then return end
             -- The deepcopy here shouldn't be necessary, indeed it shouldn't do
@@ -149,12 +126,11 @@ local function render(ctx)
             else
                ctx.message:fragment({ "fa.ui-blueprints-copy-failed" })
             end
-         end)
-      )
+         end,
+      })
 
-      table.insert(
-         menu_items,
-         MenuItems.clickable_label("delete", { "fa.ui-blueprints-menu-delete" }, function(ctx)
+      builder:add_clickable("delete", { "fa.ui-blueprints-menu-delete" }, {
+         on_click = function(ctx)
             local p = game.get_player(ctx.pindex)
             if not p then return end
 
@@ -162,54 +138,47 @@ local function render(ctx)
             bp.set_stack(nil) --calls event handler to delete empty planners.
             ctx.message:fragment({ "fa.blueprints-ui-deleted" })
             ctx.controller:close()
-         end)
-      )
+         end,
+      })
 
-      table.insert(
-         menu_items,
-         MenuItems.clickable_label("export", { "fa.ui-blueprints-menu-export" }, function(ctx)
+      builder:add_clickable("export", { "fa.ui-blueprints-menu-export" }, {
+         on_click = function(ctx)
             storage.players[ctx.pindex].blueprint_menu.edit_export = true
             Graphics.create_text_field_frame(ctx.pindex, "blueprint-edit-export", bp.export_stack())
             ctx.message:fragment({ "fa.ui-blueprints-export-txtbox" })
-            ctx.controller:close_because_textbox()
-         end)
-      )
+            ctx.controller:close_for_textbox()
+         end,
+      })
    end
 
-   table.insert(
-      menu_items,
-      MenuItems.clickable_label("import", { "fa.ui-blueprints-menu-import" }, function(ctx)
+   builder:add_clickable("import", { "fa.ui-blueprints-menu-import" }, {
+      on_click = function(ctx)
          -- For now, this shells out to legacy UI: close ourselves, and let it
          -- do the work.
-         storage.players[pindex].blueprint_menu.edit_import = true
-         Graphics.create_text_field_frame(pindex, "blueprint-edit-import")
+         storage.players[ctx.pindex].blueprint_menu.edit_import = true
+         Graphics.create_text_field_frame(ctx.pindex, "blueprint-edit-import")
          ctx.message:fragment({ "fa.ui-blueprints-import-txtbox" })
-         ctx.controller:close_because_textbox()
-      end)
-   )
+         ctx.controller:close_for_textbox()
+      end,
+   })
 
-   table.insert(
-      menu_items,
-      MenuItems.clickable_label("reselect", { "fa.ui-blueprints-menu-reselect" }, function(ctx)
+   builder:add_clickable("reselect", { "fa.ui-blueprints-menu-reselect" }, {
+      on_click = function(ctx)
          storage.players[ctx.pindex].blueprint_reselecting = true
          ctx.message:fragment({ "fa.ui-blueprints-select-first-point" })
          ctx.controller:close()
-      end)
-   )
-
-   return {
-      items = menu_items,
-   }
+      end,
+   })
+   return builder:build()
 end
 
 mod.blueprint_menu_tabs = TabList.declare_tablist({
    ui_name = UiRouter.UI_NAMES.BLUEPRINT,
    tabs = {
-      Menu.declare_menu({
-         tab_name = "blueprints",
-         title = { "fa.ui-blueprints-menu-title" },
+      UiKeyGraph.declare_graph({
+         name = "blueprints-menu",
          render_callback = render,
-         state_callback = Functools.functionize({}),
+         title = { "fa.ui-blueprints-menu-title" },
       }),
    },
 })
