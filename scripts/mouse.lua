59ec9d09f9f3f67ce71d277582377805f75fcad1
Fendi <LevFendi@users.noreply.github.com>
2025-06-08 14:40:47 +0300

    Update mouse.lua
    - Expose unexplained multipliers and create TODO for automating them
    - Remove unused 'dirs'
    - Fix cursor pos reference

--- scripts/mouse.lua
+++ scripts/mouse.lua
@@ -1,8 +1,13 @@
---Here: Movement of the mouse pointer on screen
+--Here: Movement of the mouse pointer on screen. This is needed for graphics sync and gun aiming support.
 --Note: Does not include the mod cursor functions!
 
+--TODO: Automate the calculations of these 3 mults by figuring out the correct rules for them.
+-- The final pixel position depends on player screen resolution, player zoom level, possibly player display scale, and these mults.
+local tile_zoom_screen_mult = 32
+local screen_edge_mult_1 = 18
+local screen_edge_mult_2 = 1.6
+
 local fa_utils = require("scripts.fa-utils")
-local dirs = defines.direction
 local Viewpoint = require("scripts.viewpoint")
 local mod = {}
 
@@ -14,17 +19,18 @@ function mod.move_mouse_pointer(position, pindex)
    local screen_center = fa_utils.mult_position({ x = screen.width, y = screen.height }, 0.5)
    local pixels = screen_center
    local offset = { x = 0, y = 0 }
+   local vp = Viewpoint.get_viewpoint(pindex)
    if players[pindex].vanilla_mode or game.get_player(pindex).game_view_settings.update_entity_selection == true then
       return
    elseif player.remote_view == true then
       --If in remote view, take the cursor position as the center point
-      offset = fa_utils.mult_position(fa_utils.sub_position(pos, player.cursor_pos), 32 * player.zoom)
+      offset = fa_utils.mult_position(fa_utils.sub_position(pos, vp:get_cursor_pos()), tile_zoom_screen_mult * player.zoom)
    elseif mod.cursor_position_is_on_screen_with_player_centered(pindex) == false then
       --If the cursor is distant, center the pointer on the player
       pos = players[pindex].position
-      offset = fa_utils.mult_position(fa_utils.sub_position(pos, player.position), 32 * player.zoom)
+      offset = fa_utils.mult_position(fa_utils.sub_position(pos, player.position), tile_zoom_screen_mult * player.zoom)
    else
-      offset = fa_utils.mult_position(fa_utils.sub_position(pos, player.position), 32 * player.zoom)
+      offset = fa_utils.mult_position(fa_utils.sub_position(pos, player.position), tile_zoom_screen_mult * player.zoom)
    end
    pixels = fa_utils.add_position(pixels, offset)
    mod.move_pointer_to_pixels(pixels.x, pixels.y, pindex)
@@ -39,14 +45,14 @@ function mod.move_pointer_to_pixels(x, y, pindex)
       and x < game.players[pindex].display_resolution.width
       and y < game.players[pindex].display_resolution.height
    then
-      -- print("setCursor " .. pindex .. " " .. math.ceil(x) .. "," .. math.ceil(y))
+      --print("setCursor " .. pindex .. " " .. math.ceil(x) .. "," .. math.ceil(y))
    end
 end
 
 --Checks if the map position of the mod cursor falls on screen when the camera is locked on the player character.
 function mod.cursor_position_is_on_screen_with_player_centered(pindex)
-   local range_y = math.floor(18 / players[pindex].zoom) --found experimentally by counting tile ranges at different zoom levels
-   local range_x = range_y * game.get_player(pindex).display_scale * 1.6 --found experimentally by checking scales
+   local range_y = math.floor(screen_edge_mult_1 / players[pindex].zoom)
+   local range_x = range_y * game.get_player(pindex).display_scale * screen_edge_mult_2
    local vp = Viewpoint.get_viewpoint(pindex)
    local cursor_pos = vp:get_cursor_pos()
    return (
