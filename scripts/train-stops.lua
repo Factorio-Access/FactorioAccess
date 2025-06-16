0e3e2d93eb13a74da6d25c09e6a9c29e3720b72f
Austin Hicks <ahicks@ahicks.io>
2025-06-15 18:29:41 -0700

    refactor: convert fa_ imports to PascalCase in train-stops.lua
    - fa_graphics → Graphics

--- scripts/train-stops.lua
+++ scripts/train-stops.lua
@@ -1,6 +1,6 @@
 --Here: Functions relating to train train stops and train scheduling from them (which is a unique mod feature)
 --Does not include event handlers
-local fa_graphics = require("scripts.graphics")
+local Graphics = require("scripts.graphics")
 local UiRouter = require("scripts.ui.router")
 
 local mod = {}
@@ -35,7 +35,7 @@ function mod.run_train_stop_menu(menu_index, pindex, clicked, other_input)
             pindex
          )
          players[pindex].train_stop_menu.renaming = true
-         local frame = fa_graphics.create_text_field_frame(pindex, "train-stop-rename")
+         local frame = Graphics.create_text_field_frame(pindex, "train-stop-rename")
          game.get_player(pindex).opened = frame
       end
    elseif index == 2 then
@@ -154,7 +154,7 @@ function mod.run_train_stop_menu(menu_index, pindex, clicked, other_input)
       else
          printout("Type in a number and press ENTER to confirm", pindex)
          players[pindex].train_limit_editing = true
-         local frame = fa_graphics.create_text_field_frame(pindex, "train-limit-edit")
+         local frame = Graphics.create_text_field_frame(pindex, "train-limit-edit")
       end
    end
 end
